0d5b3fef0eec9b50eb2d304860e525d9d585ca6c
Brian Healy <brian.francis.healy@gmail.com>
2022-09-13 16:07:55 -0500

    Add name and provenance

--- scope_phenom/__init__.py
+++ scope_phenom/__init__.py
@@ -3,7 +3,7 @@ This module contains the Scope phenomenological taxonomy.
 """
 
 __version__ = '0.2.0'
-__all__ = ["__version__", "taxonomy"]
+__all__ = ["__version__", "taxonomy", "name", "provenance"]
 
 import os
 from os.path import join
@@ -14,3 +14,6 @@ tax_path = join(os.path.dirname(__file__), 'phenomenological.yaml')
 
 with open(tax_path) as taxonomy_yaml:
     taxonomy = yaml.load(taxonomy_yaml, Loader=yaml.FullLoader)
+
+name = 'Scope Phenomenological Taxonomy'
+provenance = 'https://github.com/bfhealy/scope-phenomenology'
