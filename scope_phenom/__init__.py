6fb27390642845cd2915ebd440d815ed97585f7f
Brian Healy <brian.francis.healy@gmail.com>
2022-09-14 10:36:46 -0500

    Reorganize hierarchy

--- scope_phenom/__init__.py
+++ scope_phenom/__init__.py
@@ -2,7 +2,7 @@
 This module contains the Scope phenomenological taxonomy.
 """
 
-__version__ = '0.2.0'
+__version__ = '1.1.0'
 __all__ = ["__version__", "taxonomy", "name", "provenance"]
 
 import os
@@ -15,5 +15,5 @@ tax_path = join(os.path.dirname(__file__), 'phenomenological.yaml')
 with open(tax_path) as taxonomy_yaml:
     taxonomy = yaml.load(taxonomy_yaml, Loader=yaml.FullLoader)
 
-name = 'Scope Phenomenological Taxonomy'
+name = 'SCoPe Phenomenological Taxonomy'
 provenance = 'https://github.com/bfhealy/scope-phenomenology'
